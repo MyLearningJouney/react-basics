996377ff38c0ac2641f7781030c71fe622d043a4
Danilo Packer <danilo.kpacker@gmail.com>
2022-08-04 20:35:31 -0300

    Updated

---
+++ src/components/List/List.tsx
@@ -0,0 +1,32 @@
+import React, { SetStateAction } from 'react';
+import { ActivityItem } from '../../types/ActivityItem';
+import style from '../List/List.module.scss'
+import ListItem from './ListItem/ListItem';
+
+interface Props{
+    activities: ActivityItem[]
+    setActivities: React.Dispatch<SetStateAction<ActivityItem[]>>
+}
+
+function List ({ activities, setActivities }: Props){
+
+    return (
+        <ul className={style.todoList}>
+        {  
+            activities.map(activity => (            
+            <ListItem 
+                key={activity.id}
+                id = {activity.id}
+                activity={activity.activity} 
+                createdDate={activity.createdDate} 
+                completedDate={activity.completedDate} 
+                completed={activity.completed} 
+                setActivities = {setActivities}
+            />
+            ))
+        }
+        </ul>
+    )
+}
+
+export default List
---
+++ src/components/List/ListItem/ListItem.tsx
@@ -0,0 +1,66 @@
+import React, { SetStateAction } from 'react';
+import style from '../ListItem/ListItem.module.scss'
+import TrashIcon from '../../Icons/TrashIcon/TrashIcon'
+import PencilIcon from '../../Icons/PencilIcon/PencilIcon'
+import CheckIcon from '../../Icons/CheckIcon/CheckIcon'
+import { ActivityItem } from '../../../types/ActivityItem';
+import CancelIcon from '../../Icons/CancelIcon/CancelIcon';
+
+interface Props {
+    activity: string,
+    createdDate: string,
+    completedDate: string, 
+    completed: boolean, 
+    id: string,
+    setActivities: React.Dispatch<SetStateAction<ActivityItem[]>>
+}
+
+function ListItem ({ activity, createdDate, completedDate, completed, id, setActivities}: Props){
+
+    function handleComplete(event: React.MouseEvent<HTMLButtonElement>) {
+        event.preventDefault()
+        setActivities(prevActitivies => 
+            prevActitivies.map(activity => 
+            {
+                if (activity.id === id){
+                    return {...activity, completed:true}
+                }
+                return activity
+            }))
+    }
+    if (completed === false){
+        return (
+            <li className={style.listItem}>
+                <div className={style.listTextWrapper}>
+                    <span className={style.listActivity}>{activity}</span>
+                    <span className={style.listDate}>{createdDate.toString()}</span>
+                </div>
+                <div className={style.listIconsWrapper}>
+                    <CheckIcon 
+                        type={"todolist"} 
+                        onClick={handleComplete}
+                    />
+                    <PencilIcon type={"todolist"}/>
+                    <TrashIcon type={"todolist"}/>
+                </div>
+            </li>
+        )
+    }
+    else{
+        return (
+            <li className={style.listItem}>
+                <div className={style.listTextWrapper}>
+                    <span className={style.listActivity}>{activity}</span>
+                    <span className={style.listDate}>{createdDate.toString()}</span>
+                </div>
+                <div className={style.listIconsWrapper}>
+                    <CancelIcon type={"todolist"}/>
+                    <PencilIcon type={"hide"}/>
+                    <TrashIcon  type={"hide"}/>
+                </div>
+            </li>
+        )
+    }
+}
+
+export default ListItem
